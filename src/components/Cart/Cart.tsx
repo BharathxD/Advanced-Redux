595e174d60064b800e2738d2dd9380949c3a80e5
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-02-17 21:23:48 +0530

    Made some changes in styling

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from "react";
+import { Fragment } from "react";
 import Card from "../UI/Card";
 import classes from "./Cart.module.css";
 import CartItem from "./CartItem";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { ICartItem } from "../../types/Cart.types";
 
 type itemsType = {
@@ -25,12 +25,18 @@ const Cart: React.FC = () => {
   );
   return (
     <Card className={classes.cart}>
-      <h2>Your Shopping Cart</h2>
-      <ul>
-        {items.map((item) => {
-          return <CartItem items={item} key={item.id} />;
-        })}
-      </ul>
+      {items.length !== 0 && (
+          <ul>
+            {items.map((item) => {
+              return <CartItem items={item} key={item.id} />;
+            })}
+          </ul>
+      )}
+      {items.length === 0 && (
+        <p className={classes.empty}>
+          <span>Your cart is empty</span>
+        </p>
+      )}
     </Card>
   );
 };
